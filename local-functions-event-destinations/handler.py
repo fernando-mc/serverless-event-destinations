aeda6f291e6a970d7a0f0c621faece02d48116be
Fernando Medina Corey <fmcorey@gmail.com>
2020-03-09 12:20:20 -0700

    Add handler that fails and succeeds

--- local-functions-event-destinations/handler.py
+++ local-functions-event-destinations/handler.py
@@ -2,20 +2,12 @@ import json
 
 
 def starting(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    return response
-
-def success(event, context):
-    print("success")
-    print(event)
-
-def failure(event, context):
-    print("failed")
-    print(event)
+    if event["Success"] == True:
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body)
+        }
+        return response
+    else:
+        print("Time to fail!")
+        raise Exception("Some serious failure")
--- stage-based-destination-arns/handler.py
+++ stage-based-destination-arns/handler.py
@@ -2,12 +2,12 @@ import json
 
 
 def starting(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    return response
+    if event["Success"] == True:
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body)
+        }
+        return response
+    else:
+        print("Time to fail!")
+        raise Exception("Some serious failure")

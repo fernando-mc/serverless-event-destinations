2164b13654f8823aa61b8f11c3dd6eea26e992f5
Fernando Medina Corey <fmcorey@gmail.com>
2020-03-09 12:29:04 -0700

    Add handlers for local functions references example

--- local-functions-event-destinations/handler.py
+++ local-functions-event-destinations/handler.py
@@ -11,13 +11,3 @@ def starting(event, context):
     else:
         print("Time to fail!")
         raise Exception("Some serious failure")
-
-
-def success(event, context):
-    print("success")
-    print(event)
-
-
-def failure(event, context):
-    print("failed")
-    print(event)
